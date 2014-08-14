3f4dc0fa935db10637a4943285dec7336a78c123
smcho <prosseek@gmail.com>
2014-08-14 11:09:26 -0500

    LattitudeType added

---
+++ src/main/scala/grapevineType/LatitudeType.scala
@@ -0,0 +1,14 @@
+package grapevineType
+
+import BottomType._
+
+/**
+ * Created by smcho on 8/11/14.
+ */
+class LatitudeType extends LocationType {
+  override def set(value: Any): Unit = {}
+
+  override def fromByteArray(b: Array[Byte]): BottomType = {NoError}
+
+  override def toByteArray(goalSize: Int): Array[Byte] = {null}
+}
