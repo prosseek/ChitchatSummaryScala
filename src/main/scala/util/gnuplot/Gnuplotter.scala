a024aad4b27c380650e8baf3d6b3d34599060987
smcho <prosseek@gmail.com>
2014-08-21 09:02:23 -0500

    gnuplotter updated

--- src/main/scala/util/gnuplot/Gnuplotter.scala
+++ src/main/scala/util/gnuplot/Gnuplotter.scala
@@ -1,67 +1,59 @@
 package util.gnuplot
 
-import java.io.OutputStream
+import java.io.{File, FileWriter}
 
 import scala.collection.mutable.ArrayBuffer
-import scala.concurrent._
-import scala.io.Source
 import scala.sys.process._
 
 /**
  * Created by smcho on 8/20/14.
  */
 object Gnuplotter {
+  def getCurrentPath = new File(".").getAbsolutePath()
+
   // plot "/Users/smcho/tmp/imgs/gnuplot/mass/dense_meshes_dir/cohorts.txt.data" using 1:2 title "singles" w lp, "/Users/smcho/tmp/imgs/gnuplot/mass/dense_meshes_dir/cohorts.txt.data" using 1:3 title "aggregates" w lp
   val template = s"""set terminal pngcairo font 'DroidSerif'
-                   |set output "OUTPUT"
+                   |set output "#{GIFFILEPATH}"
                    |
                    |set xtics font ", 15"
                    |set ytics font ", 15"
                    |set key font ",18"
                    |set key top right
-                   |set xlabel "XLABEL" font ",20"
-                   |set ylabel "YLABEL" font ",20"
-                   |set title "TITLE" font ",25"
-                 """.stripMargin
+                   |set xlabel "#{XLABEL}" font ",20"
+                   |set ylabel "#{YLABEL}" font ",20"
+                   |set title "#{TITLE}" font ",25"
+                   |""".stripMargin
 
   def getGnuplotPath = "/usr/local/bin/gnuplot"
 
   /**
-   * smap : "C1" -> "hello"
+   * smap : 1 -> "hello"
    * extract 1 and hello from the map and create the gnuplot code
    *
    * @param filePath
    * @param smap
    * @return
    */
-  def getPlotCommand(filePath:String, smap:Map[String, String]) : String = {
+  def getPlotCommand(filePath:String, smap:Map[Int, String]) : String = {
     // http://stackoverflow.com/questions/4636610/regular-expression-and-pattern-matching-in-scala
-    val pattern = "C([0-9]+)".r
     val params = (ArrayBuffer[String]() /: smap) { (acc, elem) =>
-      elem._1 match {
-        case pattern(c) => acc += s"""using 1:${c} title "${elem._2}" w lp"""
-        case _ => throw new RuntimeException(s"Wrong format ${elem._1}")
-      }
+      acc += s"""using 1:${elem._1} title "${elem._2}" w lp"""
     }.toArray
     s"""plot "${filePath}" ${params.mkString(", ")}"""
   }
 
   def generateGnuplotCommands(smap:Map[String, String]): String = {
-    //val smap = Map[String, String]("OUTPUT" -> "o", "TITLE" -> "t")
-    var tmp = template
-    smap.foreach { case (a, b) =>
-      tmp = tmp.replace(a, b)
+    def selectColumnData() = {
+      val pattern = "C([0-9]+)".r
+      // find the elements C[0-9]+ format
+      smap collect { case (pattern(r), v) => (r.toInt, v) }
     }
-
-    val pattern = "C([0-9]+)".r
-    // find the elements C[0-9]+ format
-    val plots = smap filter { x =>
-      x._1 match {
-        case pattern(r) => true
-        case  _ => false
-      }
+    def templateReplacement(template:String, replacements:Map[String, String]) :String = {
+      // http://stackoverflow.com/questions/6110062/simple-string-template-replacement-in-scala-and-clojure
+      val res = (template /: replacements)((s: String, x: (String, String)) => (s"#\\{${x._1}\\}").r.replaceAllIn(s, x._2))
+      res
     }
-    tmp + "\n" + getPlotCommand(smap("FILEPATH"), plots)
+    templateReplacement(template, smap) + "\n" + getPlotCommand(smap("DATAFILEPATH"), selectColumnData)
   }
 
   def executeGnuplot(commandFile:String): Unit = {
@@ -70,27 +62,34 @@ object Gnuplotter {
     p.!!
   }
 
+  def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B =
+    try f(resource) finally resource.close()
+  def writeStringToFile(file: File, data: String, appending: Boolean = false) =
+    using(new FileWriter(file, appending))(_.write(data))
+
   // http://stackoverflow.com/questions/7195051/scala-getting-a-callback-when-an-external-process-exits
   // http://stackoverflow.com/questions/6013415/how-does-the-scala-sys-process-from-scala-2-9-work
-  def plot(config:Map[String, String]): Unit = {
-    val inputStream = new SyncVar[OutputStream];
-    val gnuplot = getGnuplotPath
-    val pb = Process(gnuplot)
-    val pio = new ProcessIO(stdin => inputStream.put(stdin),
-      stdout => Source.fromInputStream(stdout).getLines.foreach(println),
-      stderr => Source.fromInputStream(stderr).getLines.foreach(println));
-
-    //spawn { onExit(pb.exitValue())}
-
-    pb.run(pio)
-    val a = Array("set terminal gif",
-      s"""set output "${config("outputFilepath")}""",
-      s"""plot "${config("filepath")}" u 1:2 t "${config("label1")}" w line, "${config("filepath")}" u 1:3 t "${"label2"}" w line""",
-      "exit").foreach { s =>
-      inputStream.get.write((s + "\n").getBytes)
-    }
-    inputStream.get.close()
+  // http://stackoverflow.com/questions/4604237/how-to-write-to-a-file-in-scala
+  def plot[T <: AnyVal](config:Map[String, String], data:Array[Array[T]]): Unit = {
+    // 1. create the gnuplot file
+    writeStringToFile(new File(config("DATAFILEPATH")), generateData(data))
+    writeStringToFile(new File(config("GNUPLOTFILEPATH")), generateGnuplotCommands(config))
+    executeGnuplot(config("GNUPLOTFILEPATH"))
   }
 
+  /**
+   *
+   * Array(Array(1,2,3),Array(4,5,6),Array(7,8,9)) -> "1 4 7\n 2 5 8\n 3 6 9\n"
+   *
+   * @param data
+   * @return
+   */
+  def generateData[T <: AnyVal](data:Array[Array[T]]): String = {
+    def getValueString[T <: AnyVal](v:Array[T]) = v.map { value => value.toString + " "} reduce {_ + _}
 
+    val sb = new StringBuilder
+    data.transpose.foreach { ar => sb.append(s"${getValueString(ar)}\n")}
+
+    sb.toString
+  }
 }
--- src/test/scala/util/gnuplot/TestGnuplot.scala
+++ src/test/scala/util/gnuplot/TestGnuplot.scala
@@ -1,18 +1,35 @@
 package util.gnuplot
 
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
 /**
  * Created by smcho on 8/20/14.
  */
-class TestGnuplot extends FunSuite {
+class TestGnuplot extends FunSuite with BeforeAndAfter {
+  var smap:Map[String, String] = _
+  before {
+    smap = Map[String, String](
+      "GNUPLOTFILEPATH" -> s"${Gnuplotter.getCurrentPath}/experiment/tmp/gnuplot.txt",
+      "GIFFILEPATH" -> s"${Gnuplotter.getCurrentPath}/experiment/tmp/t1.gif",
+      "TITLE" -> "Dense meshes",
+      "XLABEL" -> "Node size",
+      "YLABEL" -> "Identification rate (%)",
+      "C3" -> "aggregates",
+      "DATAFILEPATH" -> s"${Gnuplotter.getCurrentPath}/experiment/tmp/data.txt.data"
+    )
+  }
+
+  test ("generateData") {
+    val data = Array(Array(1,2,3), Array(2,3,4), Array(3,4,5))
+    val result ="1 2 3 \n2 3 4 \n3 4 5 \n"
+    assert(Gnuplotter.generateData(data) == result)
+  }
   /**
    * Just plot interactive way
    */
-  test ("Simple") {
-    val m = Map[String, String]("outputFilepath" -> "experiment/tmp/hi.gif",
-      "filepath" -> "experiment/tmp/p.txt", "label1"->"Hello", "label2"->"World")
-    Gnuplotter.plot(m)
+ test ("Simple") {
+    val data = Array(Array(1,2,3), Array(2,3,4), Array(3,4,5))
+    Gnuplotter.plot(smap, data)
   }
 
   /**
@@ -26,7 +43,7 @@ class TestGnuplot extends FunSuite {
    * Get plot command
    */
   test ("getPlotCommand") {
-    val map = Map[String, String]("C1" -> "Hello", "C2" -> "World")
+    val map = Map[Int, String](1 -> "Hello", 2 -> "World")
     assert(Gnuplotter.getPlotCommand("Hello", map) == """plot "Hello" using 1:1 title "Hello" w lp, using 1:2 title "World" w lp""")
   }
 
@@ -34,14 +51,6 @@ class TestGnuplot extends FunSuite {
    * Generate gnuplot file from a map
    */
   test ("generateGif") {
-    val smap = Map[String, String](
-      "OUTPUT" -> "experiment/tmp/t1.gif",
-      "TITLE" -> "Dense meshes",
-      "XLABEL" -> "Node size",
-      "YLABEL" -> "Identification rate (%)",
-      "C3" -> "aggregates",
-      "FILEPATH" -> "experiment/tmp/identification_rate.txt.data"
-    )
     println(Gnuplotter.generateGnuplotCommands(smap))
   }
 }
