d155f6b0548ecef9f16b3ce1e57101e9206c030e
smcho <prosseek@gmail.com>
2014-08-25 12:05:03 -0500

    added gnuplot for size

---
+++ src/test/scala/experiment/DrawGnuplotForSize.scala
@@ -0,0 +1,24 @@
+package experiment
+
+import util.experiment.Run
+import util.gnuplot.Gnuplotter
+
+/**
+ * Created by smcho on 8/20/14.
+ */
+object DrawGnuplotForSize extends App with GnuplotMaps {
+
+  def testIt(message:String) { // } () {
+    println(message)
+    //var arrays = Run.getSizes(summaryPath="experiment/contextsForTest/summary1.txt")
+    //Gnuplotter.plot(getMapForSize("summary1"), arrays)
+
+//    val arrays = Run.getSizes(summaryPath="experiment/contextsForTest/summary2.txt")
+//    Gnuplotter.plot(getMapForSize("summary2"), arrays)
+
+    val arrays = Run.getSizes(summaryPath="experiment/scenario/s4.txt")
+    Gnuplotter.plot(getMapForSize("scenario4"), arrays)
+  }
+
+  testIt("Simple test: bytes vs size")
+}
