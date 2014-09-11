70988b144e48531d2028bf5d55181d0455dcf518
smcho <prosseek@gmail.com>
2014-09-11 10:53:05 -0500

    range checker

---
+++ src/main/scala/app/RangeOfAlphaTest.scala
@@ -0,0 +1,34 @@
+package app
+
+import core.BloomierFilterSummary
+
+import scala.collection.mutable.{Map => MMap}
+
+/**
+ * Created by smcho on 9/11/14.
+ */
+object RangeOfAlphaTest extends App {
+  var nmax = Int.MinValue
+  var nmin = Int.MaxValue
+  var mmax = Int.MinValue
+  var mmin = Int.MaxValue
+  var alphasum = 0.0
+  val count = 100
+
+  def calculate2(i:Int, bf: BloomierFilterSummary) : Unit = {
+    val n = bf.getN()
+    val m = bf.getM()
+    //println(s"${n}-${m} => ${m.toDouble/n}")
+    if (n > nmax) nmax = n
+    if (n < nmin) nmin = n
+    if (m > mmax) mmax = m
+    if (m < mmin) mmin = m
+    alphasum += (m.toDouble)/n
+  }
+
+  val conf = MMap[String, Any]()
+  conf("iteration") = count
+  conf("byteWidth") = -1 // random byte width
+  GenerateContexts.parallelExecute(configuration = conf.toMap, calculate2)
+  println(s"N(${nmin} - ${nmax}) M(${mmin} - ${nmax}) ALPHA AVG ${alphasum/count})")
+}
