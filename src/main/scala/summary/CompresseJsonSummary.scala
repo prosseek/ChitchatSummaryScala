bea467cee03dedbe032157900ca372bf93640e4d
smcho <sm.cho@mac.com>
2016-04-06 17:28:15 -0500

    CompressedJson implemented

--- src/main/scala/summary/CompresseJsonSummary.scala
+++ src/main/scala/summary/CompresseJsonSummary.scala
@@ -1,18 +1,32 @@
 package summary
 
-object CompresseJSonSummary {
+import util.compression.Compressor
+import util.json.Json
+
+object CompresseJsonSummary {
   def name = "compressed_json"
   def apply(filePath:String) = {
-    val c = new CompresseJSonSummary
+    val c = new CompresseJsonSummary
     c.loadJson(filePath)
     c
   }
 }
 
-class CompresseJSonSummary extends JsonSummary {
-  override def name = CompresseJSonSummary.name
-  override def serialize = {
-    util.compression.Compressor.compress(super.serialize)
+class CompresseJsonSummary extends JsonSummary {
+  override def name = CompresseJsonSummary.name
+
+  override def serialize : Array[Byte] = {
+    val compressed = Compressor.compress(super.serializedContent)
+    _serialize(CompresseJsonSummary.name, compressed)
   }
-  override def deserialize(ba: Array[Byte]) = ???
+  override def deserialize(ba: Array[Byte]) : Map[String, Any] = {
+    val (name, content) = _deserialize(ba)
+    if (name == CompresseJsonSummary.name) {
+      val decompressed = Compressor.decompress(content)
+      val str = new String(decompressed)
+      Json.parse(str)
+    }
+    else
+      throw new RuntimeException(s"Wrong serialized file format: ${CompresseJsonSummary.name} required")
+  }
 }
